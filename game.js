8704a2222be98cb90c46234bf88921b8b3e63dbd
RzWilliam <90561147+RzWilliam@users.noreply.github.com>
2022-11-17 22:07:16 +0100

    :rocket: add some part

---
+++ game.js
@@ -0,0 +1,11 @@
+let submit = document.querySelector('#submitPassword');
+let password = document.querySelector('#password');
+let error = document.querySelector('#error')
+
+submit.addEventListener('click', e => {
+    if(password.value == '4Vru/b5c$6tqsEd@SCTym%6b7'){
+        document.location.href = 'Hello';
+    } else {
+            error.innerText = "Mot de passe incorrect"
+    }
+})
